d97b265bc1d5b258686a323515ed0dc6b0dd73a8
alan vieira <alanvieirawe89@gmail.com>
2022-12-06 20:31:18 -0300

    projeto htm

---
+++ atividade sql.sql
@@ -0,0 +1,17 @@
+select * from animal;
+ 
+ INSERT INTO animal VALUES
+    (1, 'rex' , 'pitbull' , 'macho' , 'preto');
+	
+select * from animal;
+ 
+ INSERT INTO animal VALUES
+    (2, 'mel' , 'vira lata' , 'fêmea' , 'branca');
+	
+	
+select * from dono;
+ 
+ INSERT INTO dono VALUES
+    (1, 'livia' , ' vila nova' , 'cachoeiro de itapemirim');
+
+	
